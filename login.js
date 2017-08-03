005925bd2de9db4d79e1c82bc491fee300352306
José Jailton <josejailton@gmail.com>
2017-08-03 14:22:10 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,40 @@
-new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue.js!'
-  }
+Vue.component('login', {
+  template: `<div class="card-deck">
+  <div class="card">
+    <a href="link"><img class="card-img-top rounded mx-auto d-block" src="img/profile_pic.jpg" alt="Card image cap"></a>
+    <a href="link"><div class="card-block">
+      <h4 class="card-title">Leddy</h4>
+      <p class="card-text"></p>
+    </div></a>
+    <div class="card-footer">
+      <small class="text-muted">Ultimo acesso a 3 mins<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true" class="del_user_localstorage">&times;</span>
+  </button></small>
+    </div>
+  </div>
+  <div class="card">
+    <a href="link"><img class="card-img-top rounded mx-auto d-block" src="img/profile_pic.jpg" alt="Card image cap"></a>
+    <a href="link"><div class="card-block">
+      <h4 class="card-title">Gilliard</h4>
+      <p class="card-text"></p>
+    </div></a>
+    <div class="card-footer">
+      <small class="text-muted">Ultimo acesso a 5 mins<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true" class="del_user_localstorage">&times;</span>
+  </button></small>
+    </div>
+  </div>
+  <div class="card">
+    <a href="link"><img class="card-img-top rounded mx-auto d-block" src="img/profile_pic.jpg" alt="Card image cap"></a>
+    <a href="link"><div class="card-block">
+      <h4 class="card-title">Miqueias</h4>
+      <p class="card-text"></p>
+    </div></a>
+    <div class="card-footer">
+      <small class="text-muted">Ultimo acesso a 1.5 mins<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true" class="del_user_localstorage">&times;</span>
+  </button></small>
+    </div>
+  </div>
+</div>`
 })
